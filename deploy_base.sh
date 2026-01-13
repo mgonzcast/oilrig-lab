0ab55decbcf5d9b8bb30103aac773d47f651bbfa
mgonzcast <63349279+mgonzcast@users.noreply.github.com>
2026-01-13 09:26:48 +0100

    Delete deploy_base.sh

--- deploy_base.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-vagrant box remove windows-server-2019
-rm -f windows-server-2019-virtualbox.box
-packer build windows-server-2019.pkr.hcl
-vagrant box add windows-server-2019 windows-server-2019-virtualbox.box 
